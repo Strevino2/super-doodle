d59ba594f67c5152eb00456df886564374ae10f4
Strevino2 <strevino2@hotmail.com>
2022-01-27 22:10:30 -0600

    add JS

---
+++ node.js
@@ -0,0 +1,25 @@
+const date = new Date("02/01/2022"); // New Date object
+date.setDate(date.getDate() + 0) // Current day plus 30 days
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+// convert target date to a date object
+let targetDate = date;
+
+// set up the loop to update the countdown once a second
+let x = setInterval(function() {
+
+  // get the current time
+  let now = new Date().getTime();
+
+  // get the time remaining
+  let distance = targetDate - now;
+
+  // update the <span> tags with the countdown values
+  document.querySelector('#days').innerHTML = Math.floor(distance / (day))+" days, ";
+  document.querySelector('#hours').innerHTML = Math.floor((distance % (day)) / (hour))+" hours, ";
+  document.querySelector('#minutes').innerHTML = Math.floor((distance % (hour)) / (minute))+" minutes, ";
+  document.querySelector('#seconds').innerHTML = Math.floor((distance % (minute)) / second)+" seconds ";
+}, second)
